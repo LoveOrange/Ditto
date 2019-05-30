b1ce2526a7d93fc0e01e4ccfc654361de577c35a
Linsama <orange24k@hotmail.com>
2019-05-30 08:03:04 +0800

    init

---
+++ src/main/java/im/linsama/ditto/Ditto.java
@@ -0,0 +1,17 @@
+package im.linsama.ditto;
+
+/**
+ * Hello world!
+ */
+public final class Ditto {
+    private Ditto() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/im/linsama/ditto/DittoTest.java
@@ -0,0 +1,18 @@
+package im.linsama.ditto;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class DittoTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
